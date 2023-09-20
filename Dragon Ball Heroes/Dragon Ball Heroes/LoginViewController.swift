921c598ed423b5606ac31240864958769664ad1d
Carlos Torrejon <carlostc22@gmail.com>
2023-09-20 18:39:28 +0200

    Started login screen development; added email and password fields, login button, and configured autolayout

---
+++ Dragon Ball Heroes/Dragon Ball Heroes/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Dragon Ball Heroes
+//
+//  Created by Juan Carlos Torrejón Cañedo on 20/9/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+
+}
+
