0f7929e754276175e8e4bc8d71b937b51a5932e8
Carlos Torrejon <carlostc22@gmail.com>
2023-09-20 17:54:51 +0200

    Initiated the project, removed main.storyboard, and created View

---
+++ Dragon Ball Heroes/Dragon Ball Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dragon Ball Heroes
+//
+//  Created by Juan Carlos Torrejón Cañedo on 20/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
