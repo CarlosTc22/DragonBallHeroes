5033b0e396f8d2ed03b9f2b560d2760d4f9baa41
Carlos Torrejon <carlostc22@gmail.com>
2023-09-22 00:46:35 +0200

    Created login screen with username and password fields, login button, and background image from file

--- Dragon Ball Heroes/Dragon Ball Heroes/LoginViewController.swift
+++ Dragon Ball Heroes/Dragon Ball Heroes/LoginViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func didTapLogin(_ sender: UIButton) {
+    }
+    
+    
 }
 
