52b548252130ad73b1cf520944ff8aef45491630
Carlos Torrejon <carlostc22@gmail.com>
2023-10-01 17:51:35 +0200

    fix login

--- Dragon Ball Heroes/Dragon Ball Heroes/Network/HTTPClient.swift
+++ Dragon Ball Heroes/Dragon Ball Heroes/Network/HTTPClient.swift
@@ -14,7 +14,11 @@ final class HTTPClient {
         case invalidResponse
         case invalidToken
         case decodingFailed
+        case unknown
+        case noData
+        case statusCode(code: Int?)
     }
+
 
     enum HTTPMethod: String {
         case post = "POST"
@@ -82,10 +86,10 @@ final class HTTPClient {
 
     func login(requestData: LoginRequest, completion: @escaping (Result<String, NetworkError>) -> Void) {
         let path = "/api/auth/login"
-        guard let url = URL(string: apiURL + path) else { return completion(.failure(.invalidURL)) }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = HTTPMethod.post.rawValue
+        guard let url = URL(string: apiURL + path) else {
+            completion(.failure(.invalidURL))
+            return
+        }
 
         let loginString = String(format: "%@:%@", requestData.username, requestData.password)
         guard let loginData = loginString.data(using: .utf8) else {
@@ -94,15 +98,35 @@ final class HTTPClient {
         }
 
         let base64LoginString = loginData.base64EncodedString()
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
         request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-        let urlSession = URLSession.shared
-
-        let task = urlSession.dataTask(with: request) { data, response, error in
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
             DispatchQueue.main.async {
-                guard let data = data, let token = String(data: data, encoding: .utf8) else {
+                guard error == nil else {
+                    completion(.failure(.unknown))
+                    return
+                }
+                
+                guard let data = data else {
+                    completion(.failure(.noData))
+                    return
+                }
+                
+                let urlResponse = response as? HTTPURLResponse
+                let statusCode = urlResponse?.statusCode
+                
+                guard statusCode == 200 else {
+                    completion(.failure(.statusCode(code: statusCode)))
+                    return
+                }
+                
+                guard let token = String(data: data, encoding: .utf8) else {
                     completion(.failure(.decodingFailed))
                     return
                 }
+                
                 HTTPClient.token = token
                 completion(.success(token))
             }
--- Dragon Ball Heroes/Dragon Ball Heroes/Screens/Login/LoginViewController.swift
+++ Dragon Ball Heroes/Dragon Ball Heroes/Screens/Login/LoginViewController.swift
@@ -8,23 +8,28 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var loginButton: UIButton!
-
+    
     let client = HTTPClient()
     
     @IBAction func didTapLogin(_ sender: UIButton) {
         guard let email = emailTextField.text, let password = passwordTextfield.text else { return }
         client.login(requestData: LoginRequest(username: email, password: password)) { result in
-            guard case .success = result else { return }
-            let heroes = DragonBallHeroesViewController(nibName: "HeroesViewController", bundle: nil)
-            let navigationController = UINavigationController(rootViewController: heroes)
-            self.view.window?.rootViewController = navigationController
+            switch result {
+            case .success(let token):
+                // Inicio de sesión exitoso, avanzar a la siguiente pantalla
+                let heroes = DragonBallHeroesViewController(nibName: "HeroesViewController", bundle: nil)
+                let navigationController = UINavigationController(rootViewController: heroes)
+                self.view.window?.rootViewController = navigationController
+            case .failure(let error):
+                // Inicio de sesión fallido, mostrar un mensaje al usuario
+                let alert = UIAlertController(title: "Error", message: "Inicio de sesión fallido. Por favor, inténtalo de nuevo.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
         }
-
     }
-
 }
-
